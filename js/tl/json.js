9db98b21cb23d2ca1f1a0f519bca292e76b7b4cb
smismi <smirnov.ru@gmail.com>
2013-05-07 09:07:30 +0400

    пишу валидатр ajax 01

---
+++ js/tl/json.js
@@ -0,0 +1,36 @@
+    {
+        _id: 1,
+        color: "#f00"
+    },
+    {
+        _id: 2,
+        color: "#0f0"
+    },
+    {
+        _id: 3,
+        color: "#ff6"
+    },
+    {
+        _id: 4,
+        color: "#066"
+    },
+    {
+        _id: 5,
+        color: "#6f0"
+    },
+    {
+        _id: 6,
+        color: "#0ff"
+    },
+    {
+        _id: 7,
+        color: "#00f"
+    },
+    {
+        _id: 7,
+        color: "#00f"
+    },
+    {
+        _id: 7,
+        color: "#00f"
+    }
